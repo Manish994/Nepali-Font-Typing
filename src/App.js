57afbd7e449ca3d1570b4364afbc671ed5c17273
Manish <manish@gmail.com>
2023-03-15 20:19:11 +0545

    English To Nepali Font Typing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import googleTransliterate from "@manish-pokhrel-font";
 
 function App() {
+  const [value, setValue] = React.useState("");
+  //let sourceText = "nepal";
+  let inputLanguage = "ne-t-i0-und";
+  let maxResult = 8;
+  let request = new XMLHttpRequest();
+  const [googleFontInput, setGoogleFontInput] = React.useState();
+
+  function getEnglishToNepaliFont() {
+    googleTransliterate(request, value, inputLanguage, maxResult).then(
+      function (response) {
+        if (response.constructor === Array && response.length > 0) {
+          debugger;
+          let res = [];
+          response.forEach((item) => res.push(item[0]));
+          console.log(res);
+          setValue(res[0]);
+        }
+      }
+    );
+  }
+
+  function handleChange(e) {
+    let res = e.target.value;
+    setValue(res);
+  }
+  function handleSpace(e) {
+    if (e.code === "Space" || e.keyCode === 32) {
+      console.log(value);
+      getEnglishToNepaliFont();
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <br></br>
+      <br></br>
+      <br></br>
+      <input
+        type="text"
+        value={value}
+        onChange={handleChange}
+        onKeyDown={handleSpace}
+      ></input>
     </div>
   );
 }
